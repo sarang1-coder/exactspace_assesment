b7889c3e97be0acae2d4df0f4f7d9155e654d7d1
LinkedLst <sarang15mar@gmail.com>
2023-08-30 17:38:45 +0530

    Main

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import Main from './Components/Main';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { ToastContainer } from 'react-toastify';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <ToastContainer />
+      <div className="App bg-black">
+      <h1 className='p-3 mb-2 bg-secondary text-white'>
+        <i>Hi Welcome to Photo Gallery</i>
+      </h1>
+      <Main/>
     </div>
+    </>
+
+    
+
   );
 }
 
